21fb50e8e44310fe96bc9ace8adc52134c0e6951
changhongda <changhongda@111.com.cn>
2021-03-23 13:13:48 +0800

    '问题修改'

--- task/part2/task2.js
+++ task/part2/task2.js
@@ -5,4 +5,4 @@ if (true) {
 	let temp;
 }
 // 执行结果是报错：Cannot access 'temp' before initialization
-// 因为通过let声明创建了一个块级作用域,块级作用域中的变量只能在声明以后才能使用,否则报错。这里 console.log打印的是块级作用域中的tmp,应该在声明之后使用
+// 因为通过let声明创建了一个块级作用域,let声明会行成一个暂时性死区,块级作用域中的变量只能在声明以后才能使用,否则报错。这里 console.log打印的是块级作用域中的tmp,应该在声明之后使用
