6bccb32e015dc936d6a76dc434e3aa5c8103d811
changhongda <963515608@qq.com>
2021-03-30 23:46:21 +0800

    webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,14 @@
 import createHeading from './heading.js';
+import './index.css'
+import icon from './icon.png';
+import about from './about.md'
 
 const heading = createHeading();
 
 document.body.append(heading);
+
+const img = new Image();
+img.src = icon;
+document.body.append(img);
+
+document.body.append(about)
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,8 +2,38 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
+    mode:'none',
 	output: {
 		filename: 'main.js',
 		path: path.join(__dirname, 'dist'),
+        publicPath:'dist/'
 	},
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.png$/,
+                use:{
+                    loader:'url-loader',
+                    options:{limit:102400}
+                }
+            },
+            {
+                test:/\.md$/,
+                use:'./mackdown-loader.js'
+            }
+        ]
+    }
 };
