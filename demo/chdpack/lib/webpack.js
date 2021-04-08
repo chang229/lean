a31fa51801eb1cfbd59ca4d6c178bc2b821458e1
changhongda <changhongda@111.com.cn>
2021-04-08 13:24:18 +0800

    手写webpack

---
+++ demo/chdpack/lib/webpack.js
@@ -0,0 +1,26 @@
+const Compiler = require('./compiler');
+const NodeEnvironmentPlugin = require('./node/nodeEnvironmentPlugin');
+
+const webpack = (options) => {
+	// 实例化compiler对象
+	let compiler = new Compiler(options.context);
+	compiler.options = options;
+
+	// 初始化NodeEnvironmentPlugin(让compiler具有文件的读写能力)
+	new NodeEnvironmentPlugin().apply(compiler);
+
+	// 挂载所有plugins插件到compiler对象上
+	if (options.plugin && Array.isArray(options.plugin)) {
+		for (let plugin of options.plugin) {
+			plugin.apply(compiler);
+		}
+	}
+
+	// 挂载所有webpack内置的插件（入口）
+	// compiler.options = new WebpackOptionsApply().process(options,compiler);
+
+	// 返回compiler对象
+	return compiler;
+};
+
+module.exports = webpack;
--- demo/run.js
+++ demo/run.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack');
-
+// const webpack = require('webpack');
+const webpack = require('./chdpack');
 const config = require('./webpack.config');
 
 let compiler = webpack(config);
 
-compiler.run(function(err,stats){
-    console.log(err);
-    console.log(stats.toJson())
-})
+compiler.run(function (err, stats) {
+	console.log(err);
+	console.log(stats.toJson());
+});
