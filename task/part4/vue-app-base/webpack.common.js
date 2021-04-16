6b8abaea736d77fe4f7890c5b2d8853b0a63deb8
changhongda <changhongda@111.com.cn>
2021-04-16 16:48:11 +0800

    webpack配置

--- task/part4/vue-app-base/webpack.common.js
+++ task/part4/vue-app-base/webpack.common.js
@@ -1,81 +1,63 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry:'./src/main.js',
-    output:{
-        filename:'[name]-[contenthash:5].js',
-        path:path.resolve('dist'),
-    },
-    mode:'development',
-    // devtool:'cheap-module-evel-source-map',
-    module:{
-        rules:[
-            {
-                test: /\.vue$/,
-                use:'vue-loader'
-            },
-            {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test:/\.less$/,
-                use:['style-loader','css-loader','less-loader']
-            },
-            {
-                test:/\.css$/,
-                use:['style-loader','css-loader']
-            },
-            {
-                test:/\.(jpg|png|jpeg|gif)$/,
-                use:{
-                        loader:'url-loader',
-                        options:{
-                            limit:10240
-                        }
-                    }
-            },
-            {
-                test:/\.(ttf|eot|svg|woff|woff2)$/,
-                use:'file-loader'
-            }
-        ]
-    },
-    plugins:[
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            title:'index',
-            template: './public/index.html',
-            url: './',  //需要这里传参
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-            }
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.posix.join(
-                        path.resolve(__dirname, "public").replace(/\\/g, "/"),
-                        "**/*"
-                    ),
-                    globOptions: {
-                        ignore:['*.html']
-                    }
-                }
-            ],
-        })
-    ]
-}
+	entry: './src/main.js',
+	output: {
+		filename: '[name]-[contenthash:5].js',
+		path: path.resolve('dist'),
+		assetModuleFilename: 'images/[hash][ext][query]',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				use: 'vue-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.less$/,
+				use: ['style-loader', 'css-loader', 'less-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(jpg|png|jpeg|gif)$/,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(ttf|eot|svg|woff|woff2)$/,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		// new MiniCssExtractPlugin({
+		// 	filename: 'css/[name]-[contenthash:5].css',
+		// }),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			title: 'index',
+			template: './public/index.html',
+			url: './', //需要这里传参
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true,
+			},
+		}),
+	],
+};
--- task/part4/vue-app-base/webpack.dev.js
+++ task/part4/vue-app-base/webpack.dev.js
@@ -1,17 +1,17 @@
 const common = require('./webpack.common');
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 
-module.exports = merge(common,{
-    mode:'development',
-    devtool:'cheap-module-evel-source-map',
-    devserve:{
-        hot:true,
-        open:true
-    },
-    plugins:[
-        new webpack.HotModuleReplacementPlugin()
-    ]
-})
-
-
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'eval-cheap-source-map',
+	devServer: {
+		hotOnly: true,
+		open: true,
+		port: '1234',
+		inline: true,
+		contentBase: './public',
+	},
+	target: 'web',
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+});
